76d9a39e0cde82803e7b5fae22517c1951607872
GSunwoo <ace9sun@naver.com>
2025-09-30 11:10:49 +0900

    PassFindService 트랜잭션 적용

--- src/main/java/com/common/service/PassFinderService.java
+++ src/main/java/com/common/service/PassFinderService.java
@@ -2,30 +2,45 @@ package com.common.service;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
+import com.common.dto.MailInfoDTO;
 import com.common.dto.PassFindDTO;
 import com.common.mapper.IPassFinderMapper;
+import com.etc.PassFindMail;
+import com.etc.RandomPasswordGenerator;
+
+import jakarta.transaction.Transactional;
 
 public class PassFinderService {
 	
 	@Autowired
 	IPassFinderMapper passDAO;
-	
-	public Long getUser(PassFindDTO passFindDTO) {
-		return passDAO.findUser(passFindDTO);
-	}
-	
-	public int sendingNewPassword(Long memberId, String passwd) {
-		System.out.println("새로운 패스워드 보내기");
-		int result = passDAO.sendNewPw(memberId, passwd);
-		if (result == 1) {
-			System.out.println("전송 성공");
-		}
-		else {
-			System.out.println("전송 실패");
-		}
-		return result;
-	}
+	@Autowired
+	PassFindMail findPw;
+    
+    @Transactional
+    public boolean sendingNewPassword(PassFindDTO passFindDTO) {
+        Long memberId = passDAO.findUser(passFindDTO);
+        if (memberId == null) {
+            return false;
+        }
+        String email = passFindDTO.getEmailid() + "@" + passFindDTO.getEmaildomain();
+        String newPw = RandomPasswordGenerator.generatePass(12);
+        String encodedPw = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+                .encode(newPw)
+                .replace("{bcrypt}", "");
+
+        MailInfoDTO mailInfoDTO = new MailInfoDTO();
+        mailInfoDTO.setTo(email);
+        mailInfoDTO.setSubject("[Farm To You] 새로운 비밀번호가 발급되었습니다.");
+        mailInfoDTO.setContent(newPw);
+        findPw.newPassSender(mailInfoDTO);
+
+        passDAO.sendNewPw(memberId, encodedPw);
+
+        return true;
+    }
 	
 	public PassFindDTO getEmail(Long member_id) {
 		return passDAO.selectEmail(member_id);
